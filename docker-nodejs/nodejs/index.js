831abc4881b513814ccb2e01c2ffc971ce44e7d4
Mateus Gomes <magomescruz@gmail.com>
2024-07-03 22:00:59 -0300

    refactor: add function to select query

--- docker-nodejs/nodejs/index.js
+++ docker-nodejs/nodejs/index.js
@@ -1,31 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const config = {
     host: 'db',
     user: 'root',
     password: 'password',
     database: 'nodedb'
-}
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+};
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
 
 app.get("/:name", (req, res) => {
-    const sql = `INSERT INTO people(name) VALUES('${req.params.name}')`
-    connection.query(sql)
+    const sql = `INSERT INTO people(name) VALUES('${req.params.name}')`;
+    connection.query(sql);
 
-    connection.query("SELECT name FROM people", function (err, result) {
-        if (err) throw err;
-
-        let namesList = ''
-        for(let i = 0; i < result.length; i++) {
-            namesList += `\n${result[i].name}`
-        }
-
-        res.send(`<h1>Full Cycle Rocks!</h1>${namesList}`)
-    });
+    returnNames(res)
 })
 
 app.listen(port, () => {
     console.log('Rodando na porta ', port);
 })
+
+function returnNames(res){
+    connection.query("SELECT name FROM people", (err, result) => {
+        if (err) throw err;
+        let body = '<h1>Full Cycle Rocks!</h1>';
+
+        for(let i = 0; i < result.length; i++) {
+            body += `\n${result[i].name}`;
+        }
+
+        res.send(body);
+    });
+}
